058f371617ffad22823291211d63cb2415ae6a99
Nekender Shekhawat <108869998+nekender@users.noreply.github.com>
2024-12-05 19:41:21 +0530

    Fix

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,10 @@
-// lib/config.ts
-
 const getApiUrl = () => {
   // For server-side
   if (process.env.API_URL) {
     return process.env.API_URL;
   }
   
-  return 'http://localhost:8004';
+  return 'http://backend-authorization-gatewa-alb-1180704430.eu-north-1.elb.amazonaws.com';
 };
 
 export const API_URL = getApiUrl();
